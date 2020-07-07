d0cf785303ad70094b26240ac206091e23286c0f
Sameer Argade <sameer.argade@dws.com.au>
2020-07-07 18:16:20 +1000

    Initial Commit

---
+++ src/main/java/com/argade/usermanagementservice/UsermanagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.argade.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argade/usermanagementservice/UsermanagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argade.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
